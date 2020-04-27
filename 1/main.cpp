b46d0858d8be26116fb26866a2855c306493bc7d
zhuxiaomeng <zhuxiaomengjj@gmail.com>
2020-04-27 08:32:12 +0800

    compute bounding box

--- 1/main.cpp
+++ 1/main.cpp
@@ -19,160 +19,106 @@
 #include <stdio.h>
 #include <iostream>
 
-#include <osg/ref_ptr>
-#include <osgViewer/GraphicsWindow>
-#include <osgViewer/Viewer>
-#include <osg/Camera>
+#include <osg/ComputeBoundsVisitor>
 #include <osg/ShapeDrawable>
-#include <osg/StateSet>
-#include <osg/Material>
-#include <osgGA/EventQueue>
-#include <osgGA/TrackballManipulator>
+#include <osg/AnimationPath>
 #include <osg/MatrixTransform>
+#include <osg/PolygonMode>
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
 
-#include <osgText/Font>
-#include <osgText/Text>
-
-namespace osgCookBook {
-	osg::Camera* createHUDCamera(double left, double right, double
-		bottom, double top)
-	{
-		osg::ref_ptr<osg::Camera> camera = new osg::Camera;
-		camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
-		camera->setClearMask(GL_DEPTH_BUFFER_BIT);
-		camera->setRenderOrder(osg::Camera::POST_RENDER);
-		camera->setAllowEventFocus(false);
-		camera->setProjectionMatrix(
-			osg::Matrix::ortho2D(left, right, bottom, top));
-		camera->getOrCreateStateSet()->setMode(
-			GL_LIGHTING, osg::StateAttribute::OFF);
-		return camera.release();
-	}
-
-	osg::ref_ptr<osgText::Font> g_font = osgText::readFontFile("fonts/arial.ttf");
-	osgText::Text* createText(const osg::Vec3& pos, const std::string&
-		content, float size)
-	{
-		osg::ref_ptr<osgText::Text> text = new osgText::Text;
-		text->setDataVariance(osg::Object::DYNAMIC);
-		text->setFont(g_font.get());
-		text->setCharacterSize(size);
-		text->setAxisAlignment(osgText::TextBase::XY_PLANE);
-		text->setPosition(pos);
-		text->setText(content);
-		return text.release();
-	}
-
-	class PickHandler : public osgGA::GUIEventHandler
-	{
-	public:
-		// This virtual method must be overrode by subclasses.
-		virtual void doUserOperations(
-			osgUtil::LineSegmentIntersector::Intersection&) = 0;
-		virtual bool handle(const osgGA::GUIEventAdapter& ea,
-			osgGA::GUIActionAdapter& aa)
-		{
-			if (ea.getEventType() != osgGA::GUIEventAdapter::RELEASE
-				|| ea.getButton() != osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON
-				|| !(ea.getModKeyMask() & osgGA::GUIEventAdapter::MODKEY_CTRL))
-				return false;
-
-			osgViewer::View* viewer = dynamic_cast<osgViewer::View*>(&aa);
-			if (viewer)
-			{
-				osg::ref_ptr<osgUtil::LineSegmentIntersector>
-					intersector = new osgUtil::LineSegmentIntersector
-					(osgUtil::Intersector::WINDOW, ea.getX(), ea.getY());
-				osgUtil::IntersectionVisitor iv(intersector.get());
-				viewer->getCamera()->accept(iv);
-				if (intersector->containsIntersections())
-				{
-					osgUtil::LineSegmentIntersector::Intersection&
-						result = const_cast<osgUtil::LineSegmentIntersector::Intersection&>(*(intersector->getIntersections().begin()));
-					doUserOperations(result);
-				}
-			}
-			return false;
-		}
-	};
-}
-
-class RemoveShapeHandler : public osgCookBook::PickHandler
-{
-	virtual void doUserOperations(osgUtil::LineSegmentIntersector::
-		Intersection& result)
-	{
-		if (result.nodePath.size() > 0)
-		{
-			osg::Geode* geode = dynamic_cast<osg::Geode*>(
-				result.nodePath.back());
-			if (geode) geode->removeDrawable(
-				result.drawable.get());
-		}
-	}
-};
-
-class SetShapeColorHandler : public osgCookBook::PickHandler
-{
-	virtual void doUserOperations(osgUtil::LineSegmentIntersector
-		::Intersection& result)
-	{
-		osg::ShapeDrawable* shape = dynamic_cast<osg::ShapeDrawable*>
-			(result.drawable.get());
-		if (shape) shape->setColor(osg::Vec4(
-			1.0f, 1.0f, 1.0f, 2.0f) - shape->getColor());
-	}
-};
-
-osg::Node* createMatrixTransform(osg::Geode* geode,
-	const osg::Vec3& pos)
-{
-	osg::ref_ptr<osg::MatrixTransform> trans =
-		new osg::MatrixTransform;
-	trans->setMatrix(osg::Matrix::translate(pos));
-	trans->addChild(geode);
-	return trans.release();
-}
-
-class ObserveShapeCallback : public osg::NodeCallback
+class BoundingBoxCallback : public osg::NodeCallback
 {
 public:
 	virtual void operator()(osg::Node* node, osg::NodeVisitor* nv)
 	{
-		std::string content;
-		if (_drawable1.valid()) content += "Drawable 1; ";
-		if (_drawable2.valid()) content += "Drawable 2; ";
-		if (_text.valid()) _text->setText(content);
+		osg::BoundingBox bb;
+		for (unsigned int i = 0; i < _nodesToCompute.size(); ++i)
+		{
+			osg::Node* node = _nodesToCompute[i];
+			osg::ComputeBoundsVisitor cbbv;
+			node->accept(cbbv);
+			osg::BoundingBox localBB = cbbv.getBoundingBox();
+			osg::Matrix localToWorld = osg::computeLocalToWorld(
+				node->getParent(0)->getParentalNodePaths()[0]);
+			for (unsigned int i = 0; i < 8; ++i)
+				bb.expandBy(localBB.corner(i) * localToWorld);
+		}
+
+		osg::MatrixTransform* trans =
+			static_cast<osg::MatrixTransform*>(node);
+		trans->setMatrix(
+			osg::Matrix::scale(bb.xMax() - bb.xMin(), bb.yMax() - bb.yMin(),
+				bb.zMax() - bb.zMin()) *
+			osg::Matrix::translate(bb.center()));
 	}
-	osg::ref_ptr<osgText::Text> _text;
-	osg::observer_ptr<osg::Drawable> _drawable1;
-	osg::observer_ptr<osg::Drawable> _drawable2;
+	osg::NodePath _nodesToCompute;
 };
+
+osg::AnimationPath* createAnimationPath(float radius, float time)
+{
+	osg::ref_ptr<osg::AnimationPath> path =
+		new osg::AnimationPath;
+	path->setLoopMode(osg::AnimationPath::LOOP);
+	unsigned int numSamples = 32;
+	float delta_yaw = 2.0f * osg::PI / ((float)numSamples - 1.0f);
+	float delta_time = time / (float)numSamples;
+	for (unsigned int i = 0; i < numSamples; ++i)
+	{
+		float yaw = delta_yaw * (float)i;
+		osg::Vec3 pos(sinf(yaw)*radius, cosf(yaw)*radius, 0.0f);
+		osg::Quat rot(-yaw, osg::Z_AXIS);
+		path->insert(delta_time * (float)i,
+			osg::AnimationPath::ControlPoint(pos, rot));
+	}
+	return path.release();
+}
+
 
 int main(int argc, char** argv)
 {
-	osg::ref_ptr<osg::ShapeDrawable> shape = new osg::ShapeDrawable(
-		new osg::Sphere);
-	shape->setColor(osg::Vec4(1.0f, 1.0f, 0.0f, 1.0f));
-	shape->setDataVariance(osg::Object::DYNAMIC);
-	shape->setUseDisplayList(false);
+	osg::ref_ptr<osg::MatrixTransform> cessna =
+		new osg::MatrixTransform;
+	cessna->addChild(
+		osgDB::readNodeFile("cessna.osgt.0,0,90.rot"));
+	osg::ref_ptr<osg::AnimationPathCallback> apcb =
+		new osg::AnimationPathCallback;
+	apcb->setAnimationPath(createAnimationPath(50.0f, 6.0f));
+	cessna->setUpdateCallback(apcb.get());
+	osg::ref_ptr<osg::MatrixTransform> dumptruck =
+		new osg::MatrixTransform;
+	dumptruck->addChild(osgDB::readNodeFile("dumptruck.osgt"));
+	dumptruck->setMatrix(osg::Matrix::translate(0.0f, 0.0f, -100.0f));
+	osg::ref_ptr<osg::MatrixTransform> models =
+		new osg::MatrixTransform;
+	models->addChild(cessna.get());
+	models->addChild(dumptruck.get());
+	models->setMatrix(osg::Matrix::translate(0.0f, 0.0f, 200.0f));
 
-	osg::ref_ptr<osg::Geode> geode1 = new osg::Geode;
-	geode1->addDrawable(shape.get());
-	osg::ref_ptr<osg::Geode> geode2 = dynamic_cast<osg::Geode*>(
-		geode1->clone(osg::CopyOp::SHALLOW_COPY));
-	osg::ref_ptr<osg::Geode> geode3 = dynamic_cast<osg::Geode*>(
-		geode1->clone(osg::CopyOp::DEEP_COPY_ALL));
+
+	osg::ref_ptr<BoundingBoxCallback> bbcb =
+		new BoundingBoxCallback;
+	bbcb->_nodesToCompute.push_back(cessna.get());
+	bbcb->_nodesToCompute.push_back(dumptruck.get());
+
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	geode->addDrawable(new osg::ShapeDrawable(new osg::Box));
+	osg::ref_ptr<osg::MatrixTransform> boundingBoxNode =
+		new osg::MatrixTransform;
+
+	boundingBoxNode->addChild(geode.get());
+	boundingBoxNode->setUpdateCallback(bbcb.get());
+	boundingBoxNode->getOrCreateStateSet()->setAttributeAndModes(
+		new osg::PolygonMode(osg::PolygonMode::FRONT_AND_BACK,
+			osg::PolygonMode::LINE));
+	boundingBoxNode->getOrCreateStateSet()->setMode(
+		GL_LIGHTING, osg::StateAttribute::OFF);
+
 	osg::ref_ptr<osg::Group> root = new osg::Group;
-	root->addChild(createMatrixTransform(geode1.get(),
-		osg::Vec3(0.0f, 0.0f, 0.0f)));
-	root->addChild(createMatrixTransform(geode2.get(),
-		osg::Vec3(-2.0f, 0.0f, 0.0f)));
-	root->addChild(createMatrixTransform(geode3.get(),
-		osg::Vec3(2.0f, 0.0f, 0.0f)));
-
+	root->addChild(models.get());
+	root->addChild(osgDB::readNodeFile("lz.osgt"));
+	root->addChild(boundingBoxNode.get());
 	osgViewer::Viewer viewer;
-	viewer.addEventHandler(new SetShapeColorHandler);
 	viewer.setSceneData(root.get());
 	return viewer.run();
 }
