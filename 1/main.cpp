81a7a0290cbb9072878f6cc87fb1a4ea10e918ac
zhuxiaomeng <zhuxiaomengjj@gmail.com>
2020-05-03 15:29:49 +0800

    complete branch 2_8

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,42 +1,54 @@
-#include <osg/Geometry>
-#include <osg/Geode>
-#include <osg/Depth>
-#include <osg/Texture2D>
+#include <osg/MatrixTransform>
 #include <osgDB/ReadFile>
+#include <osgUtil/CullVisitor>
 #include <osgViewer/Viewer>
+
+//#include <osgUtil/PrintVisitor>
+//#include <iostream>
+
+class BillboardCallback : public osg::NodeCallback
+{
+public:
+	BillboardCallback(osg::MatrixTransform* billboard)
+		: _billboardNode(billboard) {}
+	virtual void operator()(osg::Node* node, osg::NodeVisitor* nv)
+	{
+		osgUtil::CullVisitor* cv =
+			dynamic_cast<osgUtil::CullVisitor*>(nv);
+		if (_billboardNode.valid() && cv)
+		{
+			osg::Vec3d translation, scale;
+			osg::Quat rotation, so;
+			cv->getModelViewMatrix()->decompose(translation, rotation,
+				scale, so);
+			osg::Matrixd matrix(rotation.inverse());
+			_billboardNode->setMatrix(matrix);
+		}
+		std::cout << node->libraryName() << "::"
+			<< node->className() << std::endl;
+		traverse(node, nv);
+	}
+protected:
+	osg::observer_ptr<osg::MatrixTransform> _billboardNode;
+};
 
 int main(int argc, char *argv[])
 {
-	osg::ref_ptr<osg::Texture2D> texture = new osg::Texture2D;
-	osg::ref_ptr<osg::Image> image = osgDB::readImageFile(
-		"Images/osg256.png");
-	texture->setImage(image.get());
-	osg::ref_ptr<osg::Drawable> quad =
-		osg::createTexturedQuadGeometry(osg::Vec3(),
-			osg::Vec3(1.0f, 0.0f, 0.0f), osg::Vec3(0.0f, 1.0f, 0.0f));
-	quad->getOrCreateStateSet()->setTextureAttributeAndModes(
-		0, texture.get());
-	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-	geode->addDrawable(quad.get());
-
-	osg::ref_ptr<osg::Camera> camera = new osg::Camera;
-	camera->setCullingActive(false);
-	camera->setClearMask(0);
-	camera->setAllowEventFocus(false);
-	camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
-	camera->setRenderOrder(osg::Camera::POST_RENDER);
-	camera->setProjectionMatrix(osg::Matrix::ortho2D(
-		0.0, 1.0, 0.0, 1.0));
-	camera->addChild(geode.get());
-
-	osg::StateSet* ss = camera->getOrCreateStateSet();
-	ss->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
-	ss->setAttributeAndModes(new osg::Depth(
-		osg::Depth::LEQUAL, 1.0, 1.0));
+	osg::ref_ptr<osg::MatrixTransform> billboardNode =
+		new osg::MatrixTransform;
+	billboardNode->addChild(osgDB::readNodeFile("cessna.osg"));
 
 	osg::ref_ptr<osg::Group> root = new osg::Group;
-	root->addChild(camera.get());
-	root->addChild(osgDB::readNodeFile("cessna.osg"));
+	root->addChild(billboardNode.get());
+	root->addChild(osgDB::readNodeFile("lz.osg"));
+	root->addCullCallback(
+		new BillboardCallback(billboardNode.get()));
+
+	//std::cout << "DFS Visitor traversal: " << std::endl;
+	//osgUtil::PrintVisitor pv(std::cout);
+	//root->accept(pv);
+	//std::cout << std::endl;
+
 	osgViewer::Viewer viewer;
 	viewer.setSceneData(root.get());
 	return viewer.run();
