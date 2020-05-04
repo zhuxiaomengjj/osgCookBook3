1e55884930ffeb5267b519cfcfecd2356ac1346c
zhuxiaomeng <zhuxiaomengjj@gmail.com>
2020-05-04 08:55:16 +0800

    complete branch 3_1

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,54 +1,56 @@
-#include <osg/MatrixTransform>
-#include <osgDB/ReadFile>
-#include <osgUtil/CullVisitor>
+#include <osg/Geometry>
+#include <osg/Geode>
+#include <osg/LineWidth>
+#include <osgUtil/Tessellator>
 #include <osgViewer/Viewer>
-
-//#include <osgUtil/PrintVisitor>
-//#include <iostream>
-
-class BillboardCallback : public osg::NodeCallback
-{
-public:
-	BillboardCallback(osg::MatrixTransform* billboard)
-		: _billboardNode(billboard) {}
-	virtual void operator()(osg::Node* node, osg::NodeVisitor* nv)
-	{
-		osgUtil::CullVisitor* cv =
-			dynamic_cast<osgUtil::CullVisitor*>(nv);
-		if (_billboardNode.valid() && cv)
-		{
-			osg::Vec3d translation, scale;
-			osg::Quat rotation, so;
-			cv->getModelViewMatrix()->decompose(translation, rotation,
-				scale, so);
-			osg::Matrixd matrix(rotation.inverse());
-			_billboardNode->setMatrix(matrix);
-		}
-		std::cout << node->libraryName() << "::"
-			<< node->className() << std::endl;
-		traverse(node, nv);
-	}
-protected:
-	osg::observer_ptr<osg::MatrixTransform> _billboardNode;
-};
 
 int main(int argc, char *argv[])
 {
-	osg::ref_ptr<osg::MatrixTransform> billboardNode =
-		new osg::MatrixTransform;
-	billboardNode->addChild(osgDB::readNodeFile("cessna.osg"));
+	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(8);
+	(*vertices)[0].set(0.0f, 0.0f, 0.0f);
+	(*vertices)[1].set(3.0f, 0.0f, 0.0f);
+	(*vertices)[2].set(3.0f, 0.0f, 3.0f);
+	(*vertices)[3].set(0.0f, 0.0f, 3.0f);
+	(*vertices)[4].set(1.0f, 0.0f, 1.0f);
+	(*vertices)[5].set(2.0f, 0.0f, 1.0f);
+	(*vertices)[6].set(2.0f, 0.0f, 2.0f);
+	(*vertices)[7].set(1.0f, 0.0f, 2.0f);
 
+	osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array(1);
+	(*normals)[0].set(0.0f, -1.0f, 0.0f);
+
+	osg::ref_ptr<osg::Geometry> polygon = new osg::Geometry;
+	polygon->setVertexArray(vertices.get());
+	polygon->setNormalArray(normals.get());
+	polygon->setNormalBinding(osg::Geometry::BIND_OVERALL);
+	polygon->addPrimitiveSet(new osg::DrawArrays(
+		GL_QUADS, 0, 4));
+	polygon->addPrimitiveSet(new osg::DrawArrays(
+		GL_QUADS, 4, 4));
+
+	osgUtil::Tessellator tessellator;
+	tessellator.setTessellationType(osgUtil::Tessellator::TESS_TYPE_GEOMETRY);
+	tessellator.setWindingType(osgUtil::Tessellator::TESS_WINDING_ODD);
+	tessellator.retessellatePolygons(*polygon);
+
+	osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array(1);
+	(*colors)[0].set(1.0f, 1.0f, 0.0f, 1.0f);
+	osg::ref_ptr<osg::Geometry> border = new osg::Geometry;
+	border->setVertexArray(vertices.get());
+	border->setColorArray(colors.get());
+	border->setColorBinding(osg::Geometry::BIND_OVERALL);
+	border->addPrimitiveSet(new osg::DrawArrays(
+		GL_LINE_LOOP, 0, 4));
+	border->addPrimitiveSet(new osg::DrawArrays(
+		GL_LINE_LOOP, 4, 4));
+	border->getOrCreateStateSet()->setAttribute(
+		new osg::LineWidth(5.0f));
+
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	geode->addDrawable(polygon.get());
+	geode->addDrawable(border.get());
 	osg::ref_ptr<osg::Group> root = new osg::Group;
-	root->addChild(billboardNode.get());
-	root->addChild(osgDB::readNodeFile("lz.osg"));
-	root->addCullCallback(
-		new BillboardCallback(billboardNode.get()));
-
-	//std::cout << "DFS Visitor traversal: " << std::endl;
-	//osgUtil::PrintVisitor pv(std::cout);
-	//root->accept(pv);
-	//std::cout << std::endl;
-
+	root->addChild(geode.get());
 	osgViewer::Viewer viewer;
 	viewer.setSceneData(root.get());
 	return viewer.run();
