7a87a8cacc6fbe16098865d8afda9140085da546
zhuxiaomeng <zhuxiaomengjj@gmail.com>
2020-05-05 15:47:52 +0800

    temporarilly commit

--- 1/main.cpp
+++ 1/main.cpp
@@ -2,103 +2,111 @@
 #include <osg/Geometry>
 #include <osg/Geode>
 #include <osg/MatrixTransform>
+#include <osgDB/ReadFile>
 #include <osgViewer/Viewer>
 
-osg::Node* createNeedle(float w, float h, float depth,
-	const osg::Vec4& color, float angle, double period)
-{
-	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(5);
-	(*vertices)[0].set(-h * 0.5f, 0.0f, -w * 0.1f);
-	(*vertices)[1].set(h*0.5f, 0.0f, -w * 0.1f);
-	(*vertices)[2].set(-h * 0.5f, 0.0f, w*0.8f);
-	(*vertices)[3].set(h*0.5f, 0.0f, w*0.8f);
-	(*vertices)[4].set(0.0f, 0.0f, w*0.9f);
-	osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array(1);
-	(*normals)[0].set(0.0f, -1.0f, 0.0f);
-	osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array(1);
-	(*colors)[0] = color;
-	osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
-	geom->setVertexArray(vertices.get());
-	geom->setNormalArray(normals.get());
-	geom->setNormalBinding(osg::Geometry::BIND_OVERALL);
-	geom->setColorArray(colors.get());
-	geom->setColorBinding(osg::Geometry::BIND_OVERALL);
-	geom->addPrimitiveSet(new osg::DrawArrays(
-		GL_TRIANGLE_STRIP, 0, 5));
-	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-	geode->addDrawable(geom.get());
-
-	osg::ref_ptr<osg::MatrixTransform> trans =
-		new osg::MatrixTransform;
-	trans->addChild(geode.get());
-	osg::ref_ptr<osg::AnimationPath> clockPath =
-		new osg::AnimationPath;
-	clockPath->setLoopMode(osg::AnimationPath::LOOP);
-	clockPath->insert(0.0, osg::AnimationPath::ControlPoint(
-		osg::Vec3(0.0f, depth, 0.0f), osg::Quat(angle, osg::Y_AXIS)));
-	clockPath->insert(period*0.5, osg::AnimationPath::ControlPoint(
-		osg::Vec3(0.0f, depth, 0.0f), osg::Quat(angle + osg::PI,
-			osg::Y_AXIS)));
-	clockPath->insert(period, osg::AnimationPath::ControlPoint(
-		osg::Vec3(0.0f, depth, 0.0f), osg::Quat(angle + osg::PI*2.0f,
-			osg::Y_AXIS)));
-	osg::ref_ptr<osg::AnimationPathCallback> apcb =
-		new osg::AnimationPathCallback;
-	apcb->setAnimationPath(clockPath.get());
-	trans->addUpdateCallback(apcb.get());
-	return trans.release();
-}
-
-osg::Node* createFace(float radius)
+const unsigned int g_numPoints = 400;
+const float g_halfWidth = 4.0f;
+//The first step is to initialize the ribbon geometry :
+osg::Geometry* createRibbon(const osg::Vec3& colorRGB)
 {
 	osg::ref_ptr<osg::Vec3Array> vertices =
-		new osg::Vec3Array(67);
-	(*vertices)[0].set(0.0f, 0.0f, 0.0f);
-	for (unsigned int i = 1; i <= 65; ++i)
+		new osg::Vec3Array(g_numPoints);
+	osg::ref_ptr<osg::Vec3Array> normals =
+		new osg::Vec3Array(g_numPoints);
+	osg::ref_ptr<osg::Vec4Array> colors =
+		new osg::Vec4Array(g_numPoints);
+	osg::Vec3 origin = osg::Vec3(0.0f, 0.0f, 0.0f);
+	osg::Vec3 normal = osg::Vec3(0.0f, 0.0f, 1.0f);
+	for (unsigned int i = 0; i < g_numPoints - 1; i += 2)
 	{
-		float angle = (float)(i - 1) * osg::PI / 32.0f;
-		(*vertices)[i].set(radius * cosf(angle), 0.0f,
-			radius * sinf(angle));
+		(*vertices)[i] = origin; (*vertices)[i + 1] = origin;
+		(*normals)[i] = normal; (*normals)[i + 1] = normal;
+		float alpha = sinf(osg::PI * (float)i / (float)g_numPoints);
+		(*colors)[i] = osg::Vec4(colorRGB, alpha);
+		(*colors)[i + 1] = osg::Vec4(colorRGB, alpha);
 	}
-	osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array(1);
-	(*normals)[0].set(0.0f, -1.0f, 0.0f);
-	osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array(1);
-	(*colors)[0].set(1.0f, 1.0f, 1.0f, 1.0f);
-	// Avoid color state inheriting
+	\
 	osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
+	geom->setDataVariance(osg::Object::DYNAMIC);
+	geom->setUseDisplayList(false);
+	geom->setUseVertexBufferObjects(true);
+	//Set up other options and return at the end of the createRibbon() function:
 	geom->setVertexArray(vertices.get());
 	geom->setNormalArray(normals.get());
-	geom->setNormalBinding(osg::Geometry::BIND_OVERALL);
+	geom->setNormalBinding(osg::Geometry::BIND_PER_VERTEX);
 	geom->setColorArray(colors.get());
-	geom->setColorBinding(osg::Geometry::BIND_OVERALL);
+	geom->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
 	geom->addPrimitiveSet(new osg::DrawArrays(
-		GL_TRIANGLE_FAN, 0, 67));
-	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-	geode->addDrawable(geom.get());
-	return geode.release();
+		GL_QUAD_STRIP, 0, g_numPoints));
+	return geom.release();
 }
 
+class TrailerCallback : public osg::NodeCallback
+{
+public:
+	TrailerCallback(osg::Geometry* ribbon) :
+		_ribbon(ribbon) {}
+	virtual void operator()(osg::Node* node, osg::NodeVisitor* nv);
+protected:
+	osg::observer_ptr<osg::Geometry> _ribbon;
+};
+//In the operator() method, obtain necessary values and be ready to
+//edit the vertices and normals:
+
+void TrailerCallback::operator()(osg::Node* node, osg::NodeVisitor* nv) 
+{
+	osg::MatrixTransform* trans = static_cast<osg::MatrixTransform*>(node);
+	if (trans && _ribbon.valid())
+	{
+		osg::Matrix matrix = trans->getMatrix();
+		osg::Vec3Array* vertices = static_cast<osg::Vec3Array*>(
+			_ribbon->getVertexArray());
+		osg::Vec3Array* normals = static_cast<osg::Vec3Array*>(
+			_ribbon->getNormalArray());
+		for (unsigned int i = 0; i < g_numPoints - 3; i += 2)
+		{
+			(*vertices)[i] = (*vertices)[i + 2];
+			(*vertices)[i + 1] = (*vertices)[i + 3];
+			(*normals)[i] = (*normals)[i + 2];
+			(*normals)[i + 1] = (*normals)[i + 3];
+		}
+		(*vertices)[g_numPoints - 2] = osg::Vec3(0.0f, -g_halfWidth,
+			0.0f) * matrix;
+		(*vertices)[g_numPoints - 1] = osg::Vec3(0.0f, g_halfWidth,
+			0.0f) * matrix;
+		vertices->dirty();
+		osg::Vec3 normal = osg::Vec3(0.0f, 0.0f, 1.0f) * matrix;
+		normal.normalize();
+		(*normals)[g_numPoints - 2] = normal;
+		(*normals)[g_numPoints - 1] = normal;
+		normals->dirty();
+		_ribbon->dirtyBound();
+	}
+	traverse(node, nv);
+}
 
 int main(int argc, char *argv[])
 {
-	float hour_time = 10.0f, min_time = 30.0f, sec_time = 0.0f;
-	// Hour needle devides the circle into 12 parts
-	osg::Node* hour = createNeedle(6.0f, 1.0f, -0.02f,
-		osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f), osg::PI * hour_time /
-		6.0f, 3600 * 60.0);
-	// Minute/second needle devides the circle into 60 parts
-	osg::Node* minute = createNeedle(8.0f, 0.6f, -0.04f,
-		osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f), osg::PI * min_time /
-		30.0f, 3600.0);
-	osg::Node* second = createNeedle(10.0f, 0.2f, -0.06f,
-		osg::Vec4(1.0f, 1.0f, 0.0f, 1.0f), osg::PI * sec_time /
-		30.0f, 60.0);
+	osg::Geometry* geometry = createRibbon(osg::Vec3(1.0f, 0.0f,
+		1.0f));
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	geode->addDrawable(geometry);
+	geode->getOrCreateStateSet()->setMode(GL_LIGHTING,
+		osg::StateAttribute::OFF);
+	geode->getOrCreateStateSet()->setMode(GL_BLEND,
+		osg::StateAttribute::ON);
+	geode->getOrCreateStateSet()->setRenderingHint(
+		osg::StateSet::TRANSPARENT_BIN);
+	osg::ref_ptr<osg::MatrixTransform> cessna =
+		new osg::MatrixTransform;
+	cessna->addChild(osgDB::readNodeFile("cessna.osg.0,0,90.rot"));
+	cessna->addUpdateCallback(osgCookBook::createAnimationPathCallback(50.0f, 6.0f));
+	cessna->addUpdateCallback(new TrailerCallback(geometry));
 	osg::ref_ptr<osg::Group> root = new osg::Group;
-	root->addChild(hour);
-	root->addChild(minute);
-	root->addChild(second);
-	root->addChild(createFace(10.0f));
-	//Start the viewer to see our clock running :
+	root->addChild(geode.get());
+	root->addChild(cessna.get());
+	//Start the viewer :
 	osgViewer::Viewer viewer;
 	viewer.setSceneData(root.get());
 	return viewer.run();
